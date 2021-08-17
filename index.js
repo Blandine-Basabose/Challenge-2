3401893ae3871ca0cd89cda177a9603becf41216
Blandine-Basabose <blandineusanase@gmail.com>
2021-08-17 19:47:09 +0200

    initial commit

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const emailError = document.querySelector('.error-message')
 ctaBtn.onclick = function () {
     if (email.value === "") {
         email.style.borderColor = "red"
-        emailError.textContent = 'Email is required'
+        emailError.textContent = 'Your email is empty fggsgshshujshuiui dgdchffdcfffgfgfffggfg'
         emailError.style.color = "red"
         return false
     } else if (!(email.value.includes("@"))) {
         email.style.borderColor = "red"
-        emailError.textContent = 'Provide a valid email'
+        emailError.textContent = 'Provide a valid email address'
         emailError.style.color = "red"
         return false
     }
